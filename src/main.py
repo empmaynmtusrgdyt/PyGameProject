2613cb1c2f323e047e61504ac97c89ca8f7f7362
empmaynmtusrgdyt <leammax@yandex.ru>
2025-01-19 19:06:05 +0300

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    while running:
+        # внутри игрового цикла ещё один цикл
+        # приема и обработки сообщений
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                running = False
+
+        # обновление экрана
+        pygame.display.flip()
+    pygame.quit()
