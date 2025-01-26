95aecf7ec1506c3efe2643d405eae5165e535d33
empmaynmtusrgdyt <leammax@yandex.ru>
2025-01-26 04:08:29 +0300

    Level one: class of platforms added

--- src/first_level.py
+++ src/first_level.py
@@ -2,6 +2,25 @@ import pygame
 import os
 
 
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, image_path):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def update(self, player):
+        if self.rect.colliderect(player.rect):
+            if player.rect.bottom <= self.rect.top + 5 and player.velocity.y >= 0:
+                player.rect.bottom = self.rect.top
+                player.velocity.y = 0
+
+
 pygame.init()
 size = width, height = 1500, 900
 PURPLE = (139, 0, 255)
@@ -32,6 +51,12 @@ background_image, background_rect = load_image("first_level_fone.png")
 scaled_background = pygame.transform.scale(background_image, (1500, 900))
 background_rect.topleft = (0, 0)
 
+platform1 = Platform(290, 710, 100, 70, '../data/first_level_platform.png')
+platform2 = Platform(150, 770, 100, 70, '../data/first_level_platform.png')
+platform3 = Platform(10, 830, 100, 70, '../data/first_level_platform.png')
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(platform1, platform2, platform3)
 
 running = True
 while running:
@@ -39,6 +64,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.blit(scaled_background, background_rect)
+    all_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
