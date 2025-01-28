88bb8a4c9b6f5661821afd7a19aca863db5d5e7d
empmaynmtusrgdyt <leammax@yandex.ru>
2025-01-28 22:06:20 +0300

    Jumper Game demo v-2

--- src/first_level.py
+++ src/first_level.py
@@ -2,23 +2,44 @@ import pygame
 import os
 
 
-class Platform(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, image_path):
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, image_path):
         super().__init__()
-        self.image = pygame.image.load(image_path).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (width, height))
+        self.image, _ = load_image(image_path, -1)
+        self.image = pygame.transform.scale(self.image, (50, 70))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.velocity_y = 0
+        self.is_jumping = False
+        self.move_speed = 5
+        self.gravity = 2
+        self.jump_speed = -35
+        self.on_ground = True
+        self.start_y = y
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+    def update(self):
+        self.rect.x += self.move_speed
 
-    def update(self, player):
-        if self.rect.colliderect(player.rect):
-            if player.rect.bottom <= self.rect.top + 5 and player.velocity.y >= 0:
-                player.rect.bottom = self.rect.top
-                player.velocity.y = 0
+        if self.is_jumping:
+            self.velocity_y += self.gravity
+            self.rect.y += self.velocity_y
+            if self.rect.y >= self.start_y:
+                self.rect.y = self.start_y
+                self.velocity_y = 0
+                self.is_jumping = False
+                self.on_ground = True
+        if not self.is_jumping and self.rect.y < self.start_y:
+            self.rect.y += self.gravity
+
+    def jump(self):
+        if not self.is_jumping and self.on_ground:
+            self.is_jumping = True
+            self.velocity_y = self.jump_speed
+            self.on_ground = False
+
+    def draw(self, screen, camera_x):
+        screen.blit(self.image, (self.rect.x - camera_x, self.rect.y))
 
 
 pygame.init()
@@ -50,21 +71,30 @@ def load_image(name, colorkey=None):
 background_image, background_rect = load_image("first_level_fone.png")
 scaled_background = pygame.transform.scale(background_image, (1500, 900))
 background_rect.topleft = (0, 0)
+background_width = scaled_background.get_width()
 
-platform1 = Platform(290, 710, 100, 70, '../data/first_level_platform.png')
-platform2 = Platform(150, 770, 100, 70, '../data/first_level_platform.png')
-platform3 = Platform(10, 830, 100, 70, '../data/first_level_platform.png')
+player_image_path = '../data/character1.png'
+player = Player(50, 860 - 70, player_image_path)
 
-all_sprites = pygame.sprite.Group()
-all_sprites.add(platform1, platform2, platform3)
+camera_x = 0
+scroll_speed = 5
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.blit(scaled_background, background_rect)
-    all_sprites.draw(screen)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.jump()
+
+    player.update()
+    camera_x += scroll_speed
+    for i in range(-1, 2):
+        bg_x = (i * background_width) - (camera_x % background_width)
+        screen.blit(scaled_background, (bg_x, 0))
+    player.draw(screen, camera_x)
+
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
